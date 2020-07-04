7bf4b62f0cc390da9627acbb7f7b18a3d7de6da5
Vinicius Lopes Simoes <vini.lopes.simoes@gmail.com>
2020-07-03 23:32:34 -0300

    Cinematica inversa funcionando!!

--- 04. Cinemática Inversa 5 Juntas/cinematica_inversa_5gdl_versao2.m
+++ 04. Cinemática Inversa 5 Juntas/cinematica_inversa_5gdl_versao2.m
@@ -7,11 +7,23 @@ clc
 %=========================================================================%
 
 offset3 = -pi/2;
-Th = [-pi/3; 0; 0+offset3; pi/4; pi/2];
+
+% Exemplo cin inv so com o cotovelo para baixo (total = 2 solucoes)
+% Th = [-pi/3; 0; pi/4+offset3; pi/4; pi/4]; 
+
+% Exemplo cin inv (total = 8 solucoes)
+% Th = [-pi/3; 0; 0+offset3; 0; 0];
+
+% Exemplo cin inv com o cotovelo para baixo e para cima (total = 4 solucoes)
+% Th = [-pi/3; 0; pi/4+offset3; 0; 0];
+
+% Exemplo cin inv usando t31 e t32 para resolucao de theta5 (total = 2 solucoes)
+% Th = [pi/4; 0; pi/2+offset3; pi/4; 0];
+
+Th = [-pi/3; 0; 0+offset3; pi/2; 0];
 
 cinematica_direta;
 T05_direta = T05;
-T04_direta = T04;
 
 figure(1)
 plot_manipulador_from_forward_kinematic(R01, R02, R03, R05, L)
@@ -39,21 +51,26 @@ plot_manipulador_from_forward_kinematic(R01, R02, R03, R05, L)
 % ay = Robjetivo(2,3);
 % az = Robjetivo(3,3);
 
-ax = T05(1,3);
-ay = T05(2,3);
-az = T05(3,3);
+nx = T05(1,1);
+ny = T05(2,1);
+nz = T05(3,1);
 
 ox = T05(1,2);
 oy = T05(2,2);
 oz = T05(3,2);
 
-nx = T05(1,1);
-ny = T05(2,1);
-nz = T05(3,1);
+ax = T05(1,3);
+ay = T05(2,3);
+az = T05(3,3);
 
 px = T05(1,4)-eps;
 py = T05(2,4);
 pz = T05(3,4);
+
+T05_objetivo = [nx ox ax px;
+                ny oy ay py;
+                nz oz az pz
+                0   0  0  1];
 
 %=========================================================================%
 %                          Calculo dos thetas                             %
@@ -72,6 +89,7 @@ else
 end
 theta3 = repmat(theta3,1,2);
 
+
 % Calculo de theta2
 a = L(2) - L(3)*S3;
 b_1 = L(3)*C3_1;
@@ -82,109 +100,27 @@ theta2 = [atan2(a,b_1) - acos(pz/(sqrt(a^2 + b_1^2))), ...
           atan2(a,b_1) + acos(pz/(sqrt(a^2 + b_1^2))), ...
           atan2(a,b_2) + acos(pz/(sqrt(a^2 + b_2^2)))];
 
+      
 % Calculo de theta1
 g1 = L(2)*cos(theta2) - L(3)*sin(theta2 + theta3);
 
 theta1 = atan2(py./g1, px./g1);
 
-% Calculo de theta4 e theta5
-%   So funciona bem nos seguintes casos: 
-%       * theta3  = 0, para todos os outros thetas
-%       * theta3 != 0, se theta4 = 0
 
+% Calculo de theta4
 c1 = cos(theta1);
 s1 = sin(theta1);
 c23 = cos(theta2 + theta3);
 
-% Calculo de theta4
-delta = 1e-6; % Para verificar igualdade ou nao em relacao a 0
+delta = 1e-7; % Para verificar igualdade ou nao em relacao a 0
 
-t4_L = []; % Solucoes com sqrt positivas
-t4_R = []; % Solucoes com sqrt negativas
+flagAumentou4 = false; % Indica que o numero de solucoes aumentou
 
-flagAumentou4 = false; % Indica que o numero de solucoes aumentou 
-flagSingular4 = false; % Indica uma configuracao singular do robo
+det = c23;
+verif = double(abs(det)) < delta;
 
-for i = 1:size(c23,2) 
-    if abs(c23(i)) < delta
-        flagSingular4 = true;
-        % Decide qual equacao vamos resolver (1 ou 2)
-        X = c1(i)*c23(i);
-        Y = s1(i);
-        Z = ax;
-        eq = 1;
-        if abs(X) < delta && abs(Y) < delta
-            eq = 2;
-        end
-        
-        if eq == 1
-            disp('Resolvendo equacao 1') % c1*c23*s4 + s1*c4 = ax
-            if abs(X) < delta && abs(Y) > delta
-                disp('caso i')
-                C4 = Z/Y;
-                t4_1 = atan2(-sqrt(1-C4^2), C4);
-                t4_L = [t4_L t4_1];
-                t4_2 = atan2(+sqrt(1-C4^2), C4);
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(Y) < delta && abs(X) > delta
-                disp('caso ii')
-                S4 = Z/X;
-                t4_1 = atan2(S4, sqrt(1-S4^2));
-                t4_L = [t4_L t4_1];
-                t4_2 = atan2(S4, -sqrt(1-S4^2));
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) < delta
-                disp('caso iii')
-                t4_1 = atan2(-Y, X);
-                t4_L = [t4_L t4_1];
-                t4_2 = t4_1 + pi;
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) > delta
-                disp('caso iv')
-            end
-            
-        elseif eq == 2
-            disp('Resolvendo equacao 2') % s1*c23*s4 - c1*c4 = ay
-            X = s1(i)*c23(i);
-            Y = -c1(i);
-            Z = ay;
-            if abs(X) < delta && abs(Y) > delta
-                disp('caso i')
-                C4 = Z/Y;
-                t4_1 = atan2(sqrt(1-C4^2), C4);
-                t4_L = [t4_L t4_1];
-                t4_2 = atan2(-sqrt(1-C4^2), C4);
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(Y) < delta && abs(X) > delta
-                disp('caso ii')
-                S4 = Z/X;
-                t4_1 = atan2(S4, sqrt(1-S4^2));
-                t4_L = [t4_L t4_1];
-                t4_2 = atan2(S4, -sqrt(1-S4^2));
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) < delta
-                disp('caso iii')
-                t4_1 = atan2(-Y, X);
-                t4_L = [t4_L t4_1];
-                t4_2 = t4_1 + pi;
-                t4_R = [t4_R t4_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) > delta
-                disp('caso iv')
-            end
-        end
-        
-        theta4 = [t4_L, t4_R];
-    end
-end
-
-if(flagSingular4 == false)
-    disp('Resolvendo sistema')
+if verif == 0 
+    disp('Resolvendo sistema com t13 e t23 para encontrar theta4')
     X1 = c1 .* c23;
     X2 = s1 .* c23;
     Y1 = s1;
@@ -193,9 +129,48 @@ if(flagSingular4 == false)
     Z2 = ay;
     S4 = (Z1 .* Y2 - Z2 .* Y1) ./ (X1 .* Y2 - X2 .* Y1);
     C4 = (Z2 .* X1 - Z1 .* X2) ./ (X1 .* Y2 - X2 .* Y1);
-%     S4 = (ax*c1 + ay*s1) ./ (c23);
-%     C4 = (ax*s1.*c23 - ay*c1.*c23) ./ (c23);
     theta4 = atan2(S4,C4);
+else   
+    t4_L = []; % Solucoes com sqrt positivas
+    t4_R = []; % Solucoes com sqrt negativas
+    
+    for i = 1:size(c23,2) 
+        % Decide qual equacao vamos resolver (t13 ou t23)
+        X = c1(i)*c23(i);
+        Y = s1(i);
+        Z = ax;
+        eq = 't13';
+        if abs(X) < delta && abs(Y) < delta
+            X = s1(i)*c23(i);
+            Y = -c1(i);
+            Z = ay;
+            eq = 't23';
+        end
+        
+        if abs(X) < delta && abs(Y) > delta
+            disp(['Resolvendo equacao com ' eq ' para encontrar theta4 (caso i)'])
+            C4 = Z/Y;
+            t4_1 = atan2(sqrt(1-C4^2), C4);
+            t4_L = [t4_L t4_1];
+            t4_2 = atan2(-sqrt(1-C4^2), C4);
+            t4_R = [t4_R t4_2];
+        elseif abs(Y) < delta && abs(X) > delta
+            disp(['Resolvendo equacao com ' eq ' para encontrar theta4 (caso ii)'])
+            S4 = Z/X;
+            t4_1 = atan2(S4, +sqrt(1-S4^2));
+            t4_L = [t4_L t4_1];
+            t4_2 = atan2(S4, -sqrt(1-S4^2));
+            t4_R = [t4_R t4_2];
+        elseif abs(X) > delta && abs(Y) > delta && abs(Z) < delta
+            disp(['Resolvendo equacao com ' eq ' para encontrar theta4 (caso iii)'])
+            t4_1 = atan2(-Y, X);
+            t4_L = [t4_L t4_1];
+            t4_2 = t4_1 + pi;
+            t4_R = [t4_R t4_2];
+        end
+    end
+    flagAumentou4 = true;
+    theta4 = [t4_L, t4_R];
 end
 
 if(flagAumentou4)
@@ -211,115 +186,70 @@ s1 = sin(theta1);
 c23 = cos(theta2 + theta3);
 s23 = sin(theta2 + theta3);
 c4 = cos(theta4);
+s4 = sin(theta4);
 
-t5_L = []; % Solucoes com sqrt positivas
-t5_R = []; % Solucoes com sqrt negativas
+% Achamos qual sistema linear podemos resolver
+det1 = c1.^2.*s23.^2 + (s1.*s4 - c4.*c1.*c23).^2;
+verif1 = double(det1) > delta;
 
-flagAumentou5 = false; % Indica que o numero de solucoes aumentou
-flagSingular5 = false; % Indica uma configuracao singular do robo
+det2 = s1.^2.*s23.^2 + (c1.*s4 - c4.*s1.*c23).^2;
+verif2 = double(det2) > delta;
 
-for i = 1:size(c4,2)  
-    if abs(c23(i)^2 + s23(i)^2 * c4(i)^2) < delta
-        flagSingular5 = true;
-        % Decide qual equacao vamos resolver (1 ou 2)
-        X = c23(i);
-        Y = s23(i) * c4(i);
-        Z = nz;
-        eq = 1;
-        if abs(X) < delta && abs(Y) < delta
-            eq = 2;
-        end
+det3 = c23.^2 + s23.^2 .* c4.^2;
+verif3 = double(det3) > delta;
 
-        if eq == 1
-            disp('Resolvendo equacao 1') % c23*s5 + s23*c4*c5 = nz
-            if abs(X) < delta && abs(Y) > delta
-                disp('caso i')
-                C5 = Z/Y;
-                t5_1 = atan2(sqrt(1-C5^2), C5);
-                t5_L = [t5_L t5_1];
-                t5_2 = atan2(-sqrt(1-C5^2), C5);
-                t5_R = [t5_R t5_2];
-                flagAumentou5 = true;
-            elseif abs(Y) < delta && abs(X) > delta
-                disp('caso ii')
-                S5 = Z/X;
-                t5_1 = atan2(S5, sqrt(1-S5^2));
-                t5_L = [t5_L t5_1];
-                t5_2 = atan2(S5, -sqrt(1-S5^2));
-                t5_R = [t5_R t5_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) < delta
-                disp('caso iii')
-                t5_1 = atan2(-Y, X);
-                t5_L = [t5_L t5_1];
-                t5_2 = t5_1 + pi;
-                t5_R = [t5_R t5_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) > delta
-                disp('caso iv')
-            end
-            
-        elseif eq == 2
-            disp('Resolvendo equacao 2') % -s23*c4*s5 + c23*c5 = oz
-            X = -s23(i) * c4(i);
-            Y = c23(i);
-            Z = oz;
-            if abs(X) < delta && abs(Y) > delta
-                disp('caso i')
-                C5 = Z/Y;
-                t5_1 = atan2(sqrt(1-C5^2), C5);
-                t5_L = [t5_L t5_1];
-                t5_2 = atan2(-sqrt(1-C5^2), C5);
-                t5_R = [t5_R t5_2];
-                flagAumentou5 = true;
-            elseif abs(Y) < delta && abs(X) > delta
-                disp('caso ii')
-                S5 = Z/X;
-                t5_1 = atan2(S5, sqrt(1-S5^2));
-                t5_L = [t5_L t5_1];
-                t5_2 = atan2(S5, -sqrt(1-S5^2));
-                t5_R = [t5_R t5_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) < delta
-                disp('caso iii')
-                t5_1 = atan2(-Y, X);
-                t5_L = [t5_L t5_1];
-                t5_2 = t5_1 + pi;
-                t5_R = [t5_R t5_2];
-                flagAumentou4 = true;
-            elseif abs(X) > delta && abs(Y) > delta && abs(Z) > delta
-                disp('caso iv')
-            end
-        end
-        
-        theta5 = [t5_L, t5_R];
-    end
-end
-
-if(flagSingular5 == false)
-    disp('Resolvendo sistema')
+if verif1 == 1
+    disp('Resolvendo sistema com t11 e t12 para encontrar theta5')
+    X1 = -c1 .* s23;
+    X2 = (s1 .* s4 - c4 .* c1 .* c23);
+    Y1 = -(s1 .* s4 - c4 .* c1 .* c23);
+    Y2 = -c1 .* s23;
+    Z1 = nx;
+    Z2 = ox;
+    S5 = (Z1 * Y2 - Z2 * Y1) ./ (X1 .* Y2 - X2 .* Y1);
+    C5 = (Z2 * X1 - Z1 * X2) ./ (X1 .* Y2 - X2 .* Y1);
+elseif verif2 == 1
+    disp('Resolvendo sistema com t21 e t22 para encontrar theta5')
+    X1 = -s1 .* s23;
+    X2 = -(c1 .* s4 + c4 .* s1 .* c23);
+    Y1 = (c1 .* s4 + c4 .* s1 .* c23);
+    Y2 = -s1 .* s23;
+    Z1 = ny;
+    Z2 = oy;
+    S5 = (Z1 * Y2 - Z2 * Y1) ./ (X1 .* Y2 - X2 .* Y1);
+    C5 = (Z2 * X1 - Z1 * X2) ./ (X1 .* Y2 - X2 .* Y1);
+elseif verif3 == 1
+    disp('Resolvendo sistema com t31 e t32 para encontrar theta5')
     X1 = c23;
     X2 = -s23 .* c4;
     Y1 = s23 .* c4;
     Y2 = c23;
     Z1 = nz;
     Z2 = oz;
-    S5 = (Z1 .* Y2 - Z2 .* Y1) ./ (X1 .* Y2 - X2 .* Y1);
-    C5 = (Z2 .* X1 - Z1 .* X2) ./ (X1 .* Y2 - X2 .* Y1);
-%     S5 = (nz*c23 - oz*s23.*c4) ./ (c23.^2 + s23.^2 .* c4.^2);
-%     C5 = (oz*c23 + nz*s23.*c4) ./ (c23.^2 + s23.^2 .* c4.^2);
-    theta5 = atan2(S5,C5);
+    S5 = (Z1 * Y2 - Z2 * Y1) ./ (X1 .* Y2 - X2 .* Y1);
+    C5 = (Z2 * X1 - Z1 * X2) ./ (X1 .* Y2 - X2 .* Y1);
 end
-
-if(flagAumentou5)
-    theta1 = repmat(theta1, 1, 2);
-    theta2 = repmat(theta2, 1, 2);
-    theta3 = repmat(theta3, 1, 2);
-    theta4 = repmat(theta4, 1, 2);
-end
+theta5 = atan2(S5,C5);
 
 % Todas as solucoes ate o momento
 all_q = [theta1; theta2; theta3; theta4; theta5].';
+
+% Verificamos a coerencia das solucoes e eliminamos aquelas que sao inconsistentes com a realidade
+eliminateIndex = [];
+for i = 1:size(all_q, 1)    
+    Th = all_q(i,:);
+    
+    cinematica_direta;
+    
+    eliminate = checkCinematicaDireta(T05_objetivo, Th, delta);
+    
+    if eliminate == true
+        eliminateIndex = [eliminateIndex, i];
+    end
+end
+% Elimina as solucoes incoerentes
+all_q(eliminateIndex,:) = [];
+
 %% PLOT SOLUCOES
 disp('CINEMATICA INVERSA')
 
@@ -330,6 +260,9 @@ for i=1:size(all_q,1)
     
     Th = all_q(i,:);
     cinematica_direta;
+    
+    disp('Da cinematica inversa (calculado):')
+    disp(vpa(T05))
     
     disp('Da cinematica direta (objetivo):');
     disp(vpa(T05_direta))
